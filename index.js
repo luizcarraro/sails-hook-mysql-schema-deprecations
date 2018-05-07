fbe3491993b3beb6e75ac120d5ad1fdc5739fc37
Luiz Carraro <lacarraro@gmail.com>
2018-05-06 22:19:18 -0300

    Major refactor

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 var path              = require("path");
-var printMessage      = require('print-message');
+var print             = require('print-message');
 var deprecatedColumns = require(path.resolve('deprecations/columns'));
-var MySQLService = require('./utils/mysql-service');
+var MySQLService      = require('./lib/service/mysql-service');
+var Utils             = require('./lib/utils');
+const chalk           = require('chalk');
+
+var log = sails.log;
 
 module.exports = function myHook(sails) {
 
    return {
      configure: () => {
+       print(['SCHEMA DEPRECATIONS']);
        checkForDeprecatedColumns();
      }
    };
 
    function checkForDeprecatedColumns() {
-     var MySQL = new MySQLService(sails);
+     let MySQL = new MySQLService(sails);
+     let currentSchema = Utils.getCurrentSchema(sails);
 
-     var deprecationsCheck = deprecatedColumns.map(function (deprecation) {
+     let deprecationsCheck = deprecatedColumns.map(function (deprecation) {
        return function(next) {
          return MySQL.columnExists(deprecation).then(function (result) {
-           if(result) {
-             deprecation.confirmed = true;
-           }
+           deprecation.confirmed = result;
            next(null, deprecation);
            return result;
          })
          .catch(function (err) {
-           console.log('catch MySQL.columnExists', err);
            return next(err);
          });
        }
      });
 
      async.series(deprecationsCheck, function (error, results) {
-       results = results.filter( (result) => result.confirmed );
-
        if(!results.length) {
          return;
+         log.info(chalk.green.bold('No deprecations found for this columns'));
        }
-
-       printMessage(['SCHEMA DEPRECATIONS :: :: ' + results.length]);
-
+       log.info(chalk.green.bold(':: Found ' + results.length + ' deprecated columns\n'));
        results.forEach((deprecation) => {
-         printMessage([ 'TABLE :: ' + deprecation.table, 'COLUMN :: ' + deprecation.column, 'MESSAGE :: ' + deprecation.message], {border: false, marginBottom: 1, color: 'yellow'});
+
+         let {table, column, message, removedAt} = deprecation;
+
+         if(deprecation.confirmed) {
+           print([ 'TABLE \t\t:: ' + table, 'COLUMN \t:: ' + column, 'MESSAGE \t:: ' + message], {border: false, marginBottom: 1, color: 'yellow'});
+         } else {
+           if(deprecation.removedAt) {
+             sails.log.warn(`A coluna ${column} na tabela ${currentSchema}.${table} foi depreciada em ${removedAt} e pode ser removida do arquivo deprecations/columns.js`);
+           } else {
+             sails.log.warn(`A coluna ${column} na tabela ${currentSchema}.${table} foi depreciada, porém não há registro da data de remoção no arquivo deprecations/columns.js`);
+           }
+         }
        });
      });
    }
